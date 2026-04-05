9b1db34c89b41b0ac06ca3ba92deca6823a50eda
rafbiii <rafiqlabib0515@gmail.com>
2026-04-05 22:49:34 +0700

    base vue + shadcn installed

---
+++ frontend/retogen-fe/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <HelloWorld />
+  <div>
+    <Button>Click me</Button>
+  </div>
+</template>
